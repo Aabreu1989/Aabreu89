75cd258345e2141d0a5b96de1295bbcc1181a527
Amanda Jhonnes <amandajhonnes@yahoo.com.br>
2026-08-08 04:46:27 +0100

    fix(mira): build limpo direcionado para a pasta mira

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.',
   plugins: [react()],
   build: {
     outDir: 'mira',
-    emptyOutDir: true
+    emptyOutDir: false
   }
 })
